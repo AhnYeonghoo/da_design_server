e4b90914520ffd218533862d1d19542bc62a8600
dksdudgn00 <u1016@workstation>
2022-05-11 11:50:31 +0900

    api test sh file add

---
+++ service/test_api.sh
@@ -0,0 +1,8 @@
+if [ $# -lt 1 ]; then
+        echo 'give 1 argument'
+else
+        if [ $1 = 'list' ]; then
+                curl -H "Content-type: application/json" -X POST -d '{"top_k":"10"}' http://203.255.81.132:10116/api-list >> ret
+        fi
+fi
+
